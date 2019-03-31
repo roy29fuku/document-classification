a9f2aebeb317a6ebb3bcc2803db140e0741127a2
Ryota Yamada <roy29fuku@gmail.com>
2019-03-31 16:23:10 +0900

    MOVE: move files

--- named_entity_recognition/util/word2vec.py
+++
@@ -1,8 +0,0 @@
-from .data_loader import load_sentences
-
-
-if __name__ == '__main__':
-    X_train, y_train, X_valid, y_valid, X_test, y_test = load_sentences()
-    # TODO: gensim word2vev
-    # gensim corpora dictionary
-    # などでword embedding作成？
